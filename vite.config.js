c04d6e685d8ce35220e06e6b404051888919b576
ThanhDat19 <toidabotay@gmail.com>
2023-07-27 14:41:12 +0700

    fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const isProduction = process.env.NODE_ENV === 'production';
-const base = isProduction ? '/react-education/' : '/';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: base
+  // base: "/react-education/"
 })
