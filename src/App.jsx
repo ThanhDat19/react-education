79eea4fd3329067217a30518740e96de59e25446
ThanhDat19 <toidabotay@gmail.com>
2023-07-27 14:29:58 +0700

    add: router, login and register

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,20 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+import { ContextProvider } from "./contexts/ContextProvider";
 
 function App() {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    return (
-        <>
-            <div>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-            </div>
-        </>
-    );
+  return (
+    <div className="App">
+      <ContextProvider>
+        <RouterProvider router={router} />
+      </ContextProvider>
+    </div>
+  );
 }
 
 export default App;
